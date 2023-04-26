99c0d4032845c40f25fd172f2a8039d6aa323589
Scrodinger <schrodinger@MacBook-Pro-Grigorii.local>
2023-04-26 18:35:58 +0300

    in procces

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from src.routes import main
+
+app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run(debug=True)
