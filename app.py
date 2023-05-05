a19e599d01276b4e2ab4716818602e117aa2e7eb
Scrodinger <schrodinger.grigorii@gmail.com>
2023-05-06 01:25:36 +0300

    Feature: update menu, add blur

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__, template_folder='src/templates', static_folder='src/static
 app.register_blueprint(main)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
